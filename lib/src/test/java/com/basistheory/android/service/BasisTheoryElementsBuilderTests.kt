28c7dc79f4dd258df7133419358d0142e17c8d5b
Drew Hudec <drew@basistheory.com>
2022-11-17 09:33:38 -0600

    Updates tests to assert that builder doesn't throw an exception

--- lib/src/test/java/com/basistheory/android/service/BasisTheoryElementsBuilderTests.kt
+++ lib/src/test/java/com/basistheory/android/service/BasisTheoryElementsBuilderTests.kt
@@ -2,27 +2,28 @@ package com.basistheory.android.service
 
 import kotlinx.coroutines.Dispatchers
 import org.junit.Test
-import strikt.api.expectThat
+import strikt.api.expectCatching
 import strikt.assertions.isA
+import strikt.assertions.isSuccess
 
 class BasisTheoryElementsBuilderTests {
 
     @Test
     fun `can build default instance without using any setters`() {
-        val bt = BasisTheoryElements.builder()
-            .build()
-
-        expectThat(bt).isA<BasisTheoryElements>()
+        expectCatching {
+            BasisTheoryElements.builder()
+                .build()
+        }.isSuccess().isA<BasisTheoryElements>()
     }
 
     @Test
     fun `can build instance with custom parameters`() {
-        val bt = BasisTheoryElements.builder()
-            .apiKey("api_key")
-            .apiUrl("https://my-custom-api.basistheory.com")
-            .ioDispatcher(Dispatchers.Unconfined)
-            .build()
-
-        expectThat(bt).isA<BasisTheoryElements>()
+        expectCatching {
+            BasisTheoryElements.builder()
+                .apiKey("api_key")
+                .apiUrl("https://my-custom-api.basistheory.com")
+                .ioDispatcher(Dispatchers.Unconfined)
+                .build()
+        }.isSuccess().isA<BasisTheoryElements>()
     }
 }

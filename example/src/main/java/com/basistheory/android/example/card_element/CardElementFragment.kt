058c2efbde1a07086f39d014f6eb6608646b4b97
kevin <kevin@basistheory.com>
2022-12-02 18:13:01 -0600

    wip

--- example/src/main/java/com/basistheory/android/example/card_element/CardElementFragment.kt
+++ example/src/main/java/com/basistheory/android/example/card_element/CardElementFragment.kt
@@ -1,4 +1,4 @@
-package com.basistheory.android.example.text_element
+package com.basistheory.android.example.card_element
 
 import android.graphics.Color
 import android.os.Bundle
@@ -10,24 +10,22 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.basistheory.android.example.BuildConfig
 import com.basistheory.android.example.R
+import com.basistheory.android.example.databinding.FragmentCardElementBinding
 import com.basistheory.android.example.databinding.FragmentTextElementBinding
 import com.basistheory.android.service.BasisTheoryElements
 import com.basistheory.android.view.CardNumberElement
-import com.basistheory.android.view.KeyboardType
-import com.basistheory.android.view.TextElement
 import com.google.gson.GsonBuilder
 import kotlinx.coroutines.runBlocking
 import org.threeten.bp.Instant
 import org.threeten.bp.temporal.ChronoUnit
 
-class TextElementFragment : Fragment() {
-    private lateinit var nameElement: TextElement
-    private lateinit var phoneNumberElement: TextElement
-    private lateinit var orderNumberElement: TextElement
+class CardElementFragment : Fragment() {
+    private lateinit var cardNumberElement: CardNumberElement
+
     private lateinit var tokenizeResult: TextView
     private lateinit var tokenizeButton: Button
 
-    private var _binding: FragmentTextElementBinding? = null
+    private var _binding: FragmentCardElementBinding? = null
 
     private val binding get() = _binding!!
 
@@ -37,53 +35,33 @@ class TextElementFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentTextElementBinding.inflate(inflater, container, false)
+        _binding = FragmentCardElementBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        nameElement = root.findViewById(R.id.name)
-        phoneNumberElement = root.findViewById(R.id.phoneNumber)
-        orderNumberElement = root.findViewById(R.id.orderNumber)
+        cardNumberElement = root.findViewById(R.id.cardNumber)
+
         tokenizeResult = root.findViewById(R.id.tokenizeResult)
         tokenizeButton = root.findViewById(R.id.tokenizeButton)
 
-        val digitRegex = Regex("""\d""")
-        val charRegex = Regex("""[A-Za-z]""")
-
-        phoneNumberElement.keyboardType = KeyboardType.NUMBER // illustrates that it can be set programmatically
-        phoneNumberElement.mask = listOf("+", "1", "(", digitRegex,digitRegex,digitRegex, ")", " ", digitRegex, digitRegex, digitRegex, "-", digitRegex, digitRegex , digitRegex, digitRegex )
-
-        orderNumberElement.mask = listOf(charRegex, charRegex, charRegex, "-", digitRegex, digitRegex, digitRegex)
+        cardNumberElement.addChangeEventListener {
+            if (!it.isValid && it.isComplete) {
+                cardNumberElement.textColor = Color.RED
+                tokenizeButton.isEnabled = false
+            } else {
+                cardNumberElement.textColor = Color.BLACK
+                tokenizeButton.isEnabled = true
+            }
+        }
 
         _binding?.tokenizeButton?.setOnClickListener { submit(it) }
         _binding?.setTextButton?.setOnClickListener { setText(it) }
-
-
-        subscribeToEvents()
-
         return root
     }
 
-
-    private fun subscribeToEvents() {
-        nameElement.addChangeEventListener {
-            println("Change event received: $it")
-        }
-
-        nameElement.addFocusEventListener {
-            println("Element gained focus")
-        }
-
-        nameElement.addBlurEventListener {
-            println("Element lost focus")
-        }
-    }
-
-   fun setText(button: View) {
+    fun setText(button: View) {
         assert(button.id == R.id.setTextButton)
 
-       nameElement.setText("Manually Set Name")
-       phoneNumberElement.setText("2345678900")
-       orderNumberElement.setText("ABC123")
+        cardNumberElement.setText("4242424242424242")
     }
 
     fun submit(button: View) {
@@ -105,9 +83,7 @@ class TextElementFragment : Fragment() {
                 val type = "token"
                 val data = object {
                     val staticProp = "Static Value"
-                    val name = nameElement
-                    val phoneNumber = phoneNumberElement
-                    val orderNumber = orderNumberElement
+                    val cardNumber = cardNumberElement
                 }
                 val expires_at = expirationTimestamp
             })

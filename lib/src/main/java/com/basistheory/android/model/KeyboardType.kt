b6551002c7f8a8b3792eec6dcc7f0a6a3d302e38
Drew Hudec <drew@basistheory.com>
2022-12-07 12:45:22 -0500

    Removes datetime keyboard type

--- lib/src/main/java/com/basistheory/android/model/KeyboardType.kt
+++ lib/src/main/java/com/basistheory/android/model/KeyboardType.kt
@@ -4,8 +4,7 @@ import android.text.InputType
 
 enum class KeyboardType(val inputType: Int) {
     TEXT(InputType.TYPE_CLASS_TEXT),
-    NUMBER(InputType.TYPE_CLASS_NUMBER),
-    DATETIME(InputType.TYPE_CLASS_DATETIME); // TODO: do we need this one?
+    NUMBER(InputType.TYPE_CLASS_NUMBER);
 
     companion object {
         fun fromInt(value: Int): KeyboardType =

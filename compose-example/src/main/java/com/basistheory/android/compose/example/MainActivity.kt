b57b2f9c4452f18afba5188e1076393a04f286a4
Josue <jolesa97@gmail.com>
2023-04-27 09:26:27 -0600

    updating example

--- compose-example/src/main/java/com/basistheory/android/compose/example/MainActivity.kt
+++ compose-example/src/main/java/com/basistheory/android/compose/example/MainActivity.kt
@@ -2,7 +2,6 @@ package com.basistheory.android.compose.example
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import android.view.ViewGroup
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -13,28 +12,23 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import com.basistheory.android.compose.example.ui.theme.TextElements
-import com.basistheory.android.model.KeyboardType
 import com.basistheory.android.service.BasisTheoryElements
-import com.basistheory.android.view.TextElement
-import com.basistheory.android.view.mask.ElementMask
+import com.basistheory.android.view.CardNumberElement
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
     private val bt = BasisTheoryElements.builder()
-        .apiKey("key_7jc18XCxYYozHbepjpWHqU")
+        .apiKey("YOUR_API_KEY")
         .build()
 
-    private lateinit var textElement: TextElement
+    private lateinit var cardNumberElement: CardNumberElement
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // A surface container using the 'background' color from the theme
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colors.background
@@ -49,33 +43,37 @@ class MainActivity : AppCompatActivity() {
         Column(modifier = Modifier.padding(10.dp)) {
             AndroidView(
                 factory = { context ->
-                    TextElement(context).apply {
+                    CardNumberElement(context).apply {
                         layoutParams = ViewGroup.LayoutParams(
                             ViewGroup.LayoutParams.MATCH_PARENT,
                             ViewGroup.LayoutParams.WRAP_CONTENT,
                         )
                         setPadding(5, 5, 5, 5)
-                        hint = "Social Security Number"
+                        hint = "Card Number"
                         textColor = Color.GRAY
                         removeDefaultStyles = true
-                        keyboardType = KeyboardType.NUMBER
-                        mask = ElementMask("###-##-####")
                     }
                 },
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(50.dp),
                 update = {
-                    textElement = it
-                    it.addChangeEventListener { e -> println(e) }
+                    cardNumberElement = it
+                    it.addChangeEventListener { e ->
+                        run {
+                            println(e)
+                            it.textColor =
+                                if (e.isMaskSatisfied && !e.isValid) Color.RED else Color.GRAY
+                        }
+                    }
                 }
             )
 
             Button(onClick = {
                 val tokenizeResponse = runBlocking {
                     bt.tokenize(object {
-                        val type = "token"
-                        val data = textElement
+                        val type = "card_number"
+                        val data = cardNumberElement
                     })
                 }
 
@@ -83,14 +81,6 @@ class MainActivity : AppCompatActivity() {
             }) {
                 Text(text = "Tokenize")
             }
-        }
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        TextElements {
-            ShowForm()
         }
     }
 }

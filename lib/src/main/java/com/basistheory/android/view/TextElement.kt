c3a2ca594c34936a629709ed680ebe3a7fe45ba8
Josue <jolesa97@gmail.com>
2024-02-02 14:24:02 -0600

    supporting autofillhints in xml

--- lib/src/main/java/com/basistheory/android/view/TextElement.kt
+++ lib/src/main/java/com/basistheory/android/view/TextElement.kt
@@ -42,6 +42,7 @@ import com.basistheory.android.view.method.FullyHiddenTransformationMethod
 import com.basistheory.android.view.transform.ElementTransform
 import com.basistheory.android.view.validation.ElementValidator
 
+@RequiresApi(Build.VERSION_CODES.O)
 open class TextElement @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
@@ -93,6 +94,8 @@ open class TextElement @JvmOverloads constructor(
                     textSize = getDimension(R.styleable.TextElement_android_textSize, 16f * resources.displayMetrics.scaledDensity)
 
                     hintTextColor = getColor(R.styleable.TextElement_android_textColorHint, Color.LTGRAY)
+
+                    textAutofillHints = getString(R.styleable.TextElement_android_autofillHints)?.split(",")?.toTypedArray()
 
                     typeface = resolveTypeface(
                         getInt(R.styleable.TextElement_android_typeface, 0),

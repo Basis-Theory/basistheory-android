58c31c90d03730d66b6ddfb7e5a7e905d6d4f096
Josue <jolesa97@gmail.com>
2024-07-16 11:40:31 -0600

    wip

--- lib/src/main/java/com/basistheory/android/view/SensitiveEditText.kt
+++ lib/src/main/java/com/basistheory/android/view/SensitiveEditText.kt
@@ -2,6 +2,7 @@ package com.basistheory.android.view
 
 import android.content.Context
 import android.text.Editable
+import android.text.SpannableString
 import android.text.SpannableStringBuilder
 import android.text.TextWatcher
 import androidx.appcompat.widget.AppCompatEditText
@@ -22,9 +23,15 @@ internal class SensitiveEditText(
     }
 
     override fun getText(): Editable? {
+        val stackTrace = Thread.currentThread().stackTrace
+        val indexOfBtEditText =
+            stackTrace.indexOfLast { e -> e.className == "com.basistheory.android.view.SensitiveEditText" }
+        val isInternalInvocation = stackTrace[indexOfBtEditText + 1].className.startsWith("android.widget")
+                || stackTrace[indexOfBtEditText + 1].className.startsWith("android.view")
+
         val text = super.getText()
         println("actual text: $text")
-        return if (allowTextAccess || text.isNullOrBlank()) {
+        return if (allowTextAccess || text.isNullOrBlank() || isInternalInvocation) {
             println("returning actual text")
             text
         }
@@ -33,16 +40,4 @@ internal class SensitiveEditText(
             SpannableStringBuilder("*".repeat(text.length))
         }
     }
-
-//    override fun getText(): Editable? {
-//        val stackTrace = Thread.currentThread().stackTrace
-//        val indexOfBtEditText =
-//            stackTrace.indexOfLast { e -> e.className == "com.basistheory.android.view.SensitiveEditText" }
-//        val isInternalInvocation = stackTrace[indexOfBtEditText + 1].className.startsWith("android.widget")
-//                || stackTrace[indexOfBtEditText + 1].className.startsWith("android.view")
-//
-//        return if (allowTextAccess || isInternalInvocation)
-//            super.getText()
-//        else SpannableStringBuilder("*****")
-//    }
 }

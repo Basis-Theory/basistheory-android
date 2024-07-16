b26744fd09466b815a67950b91e90a7d0e2a26f8
Josue <jolesa97@gmail.com>
2024-07-16 16:01:31 -0600

    catch array index exceptions

--- lib/src/main/java/com/basistheory/android/view/SensitiveEditText.kt
+++ lib/src/main/java/com/basistheory/android/view/SensitiveEditText.kt
@@ -6,6 +6,7 @@ import android.text.SpannableStringBuilder
 import android.text.TextWatcher
 import androidx.appcompat.widget.AppCompatEditText
 import com.basistheory.android.BuildConfig
+import java.lang.Exception
 
 internal class SensitiveEditText(
     context: Context
@@ -22,16 +23,19 @@ internal class SensitiveEditText(
     }
 
     override fun getText(): Editable? {
-        val stackTrace = Thread.currentThread().stackTrace
-        val indexOfBtEditText =
-            stackTrace.indexOfLast { e -> e.className == "com.basistheory.android.view.SensitiveEditText" }
-        val isInternalInvocation =
-            stackTrace[indexOfBtEditText + 1].className.startsWith("android.widget.") || stackTrace[indexOfBtEditText + 1].className.startsWith(
-                "android.view."
-            )
+        var isInternalInvocation: Boolean = false
+        try {
+            val stackTrace = Thread.currentThread().stackTrace
+            val indexOfBtEditText =
+                stackTrace.indexOfLast { e -> e.className == "com.basistheory.android.view.SensitiveEditText" }
+            isInternalInvocation =
+                stackTrace[indexOfBtEditText + 1].className.startsWith("android.widget.") || stackTrace[indexOfBtEditText + 1].className.startsWith(
+                    "android.view."
+                )
+        } catch (_: Exception) { }
 
         val text = super.getText()
-        return if (allowTextAccess || text.isNullOrBlank() || isInternalInvocation || BuildConfig.DEBUG) {
+        return if (allowTextAccess || text.isNullOrBlank() || isInternalInvocation) {
             text
         } else {
             SpannableStringBuilder("*".repeat(text.length))
